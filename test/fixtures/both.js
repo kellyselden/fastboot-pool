1fbd778666bc49a9a9e0a37af03e212e4f17ad35
Kelly Selden <kellyselden@gmail.com>
2017-05-10 17:44:32 -0700

    handle errors more gracefully

---
+++ test/fixtures/both.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const FastBoot = require('fastboot');
+
+const app = new FastBoot({
+  distPath: '../my-app/dist'
+});
+
+let count = 0;
+
+module.exports = function(message) {
+  if (++count % 2 === 0) {
+    return Promise.reject(new Error('Congratulations, you didn\'t make it!'));
+  }
+
+  let request = message.request;
+  let response = message.response;
+
+  return app.visit(request.url, {
+    request,
+    response
+  }).then(result => {
+    return result.html();
+  });
+};
